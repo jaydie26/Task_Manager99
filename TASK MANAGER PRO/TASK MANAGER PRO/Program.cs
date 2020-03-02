3e958f2e8b30d41b819da18966050769880ac006
jaydie26 <nguyendinhdai01@gmail.com>
2020-03-02 02:09:10 -0800

    sua

--- TASK MANAGER PRO/TASK MANAGER PRO/Program.cs
+++ TASK MANAGER PRO/TASK MANAGER PRO/Program.cs
@@ -17,6 +17,7 @@ namespace TASK_MANAGER_PRO
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            dddd;
         }
     }
 }
